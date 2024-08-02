3d9faf127d47b11eb71d220b1798077751eaf957
end3r-man <enderman@elg.foundation>
2024-08-02 15:11:25 +0530

    🏁 Dash Up

--- resources/views/livewire/dash/index-pg.blade.php
+++ resources/views/livewire/dash/index-pg.blade.php
@@ -1,3 +1,5 @@
-<div>
-    {{-- Stop trying to control. --}}
+<div class="w-full h-[90%] center">
+    <div class="w-4/5 h-4/5 rounded-md overflow-hidden bg-white center">
+        <h2 class="text-4xl w-full whitespace-nowrap text-center capitalize">Welcome Back! <span class="text-[#EF5A6F]">{{ auth()->user()->name }}</span></h2>
+    </div>
 </div>
